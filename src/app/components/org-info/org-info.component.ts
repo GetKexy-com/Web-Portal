f122c2ce4286bd497d2ac5054d85d16ed5beffac
Billal Hossain (Tousif) <tuhin.world@gmail.com>
2026-04-20 12:30:59 +0600

    Update org-info.component.ts

--- src/app/components/org-info/org-info.component.ts
+++ src/app/components/org-info/org-info.component.ts
@@ -1,56 +1,47 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "../../../environments/environment";
-import { AuthService } from "../../services/auth.service";
-import { User } from "../../models/user";
-import { constants } from "../../helpers/constants";
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user';
+import { constants } from '../../helpers/constants';
 
 @Component({
   selector: 'app-org-info',
   imports: [],
   templateUrl: './org-info.component.html',
-  styleUrl: './org-info.component.scss'
+  styleUrl: './org-info.component.scss',
 })
-export class OrgInfoComponent {
+export class OrgInfoComponent implements OnInit {
   userData: User;
 
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService) {
+  }
 
   ngOnInit() {
     this.userData = this._authService.userTokenValue;
   }
 
   get username(): string {
-    return this.userData ? this.userData.firstName + " " + this.userData.lastName : "";
+    return this.userData ? this.userData.firstName + ' ' + this.userData.lastName : '';
   }
 
   get orgName(): string {
     const type = this.userData.type;
     if (type === constants.RESTAURANT) {
-      return this.userData ? this.userData.restaurant_name : "";
+      return this.userData ? this.userData.restaurant_name : '';
     } else if (type === constants.DISTRIBUTOR) {
-      return this.userData ? this.userData.distributor_name : "";
+      return this.userData ? this.userData.distributor_name : '';
     } else if (type === constants.BRAND) {
-      return this.userData ? this.userData.supplier_name : "";
+      return this.userData ? this.userData.supplier_name : '';
     } else {
-      return "";
+      return '';
     }
   }
 
   get orgLogo(): string {
-    const type = this.userData.type;
-    let logo = "";
-    if (type === constants.RESTAURANT) {
-      logo = this.userData.restaurant_logo ? this.userData.restaurant_logo : "";
-    } else if (type === constants.DISTRIBUTOR) {
-      logo = this.userData.distributor_logo ? this.userData.distributor_logo : "";
-    } else if (type === constants.BRAND) {
-      logo = this.userData.supplier_logo ? this.userData.supplier_logo : "";
-    }
-
-    if (logo) {
-      return environment.imageUrl + logo;
+    if (this.userData.supplier_logo) {
+      return environment.imageUrl + this.userData.supplier_logo;
     } else {
-      return "/assets/images/company_default_logo.png";
+      return '/assets/images/company_default_logo.png';
     }
   }
 }
